ca9f06de8072029f67dc2a1a50937ee92fe5e32e
JackMocho <196417405+JackMocho@users.noreply.github.com>
2025-07-23 22:27:11 +0300

    adustment

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'mocho',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'housing',
-  password: process.env.DB_PASSWORD || 'jackmocho',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT) || 5432,
 });
 
